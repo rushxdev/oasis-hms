ac612d2a98446187a7a93e6b1637e07bf3ac7f83
rushxdev <rashmikapabasara@gmail.com>
2024-10-10 11:19:38 +0530

    doctorDashboard

--- Oasis/src/main/java/com/login/LoginServlet.java
+++ Oasis/src/main/java/com/login/LoginServlet.java
@@ -2,13 +2,12 @@ package com.login;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/LoginServlet")
+@SuppressWarnings("serial")
 public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
